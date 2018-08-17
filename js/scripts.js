30699511caa5a65d6c6d0c37b04f18ed818816a8
Dima K <lusdjke@gmail.com>
2018-08-17 19:05:32 +0800

    321
    123

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
+'use strict'
+
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    txt: 'toujours économique'
   }
 })
+
+var animate = (function(){
+  var i = 6;
+  var countStart = setInterval(function(){
+      i--;
+      if (i == 1) {clearInterval(countStart);}
+      document.getElementById('body').style.backgroundImage = "url('img/Fond" + i + ".jpg')";
+  }, 3000);
+})();
+
+
+
+// var countBg = function (){
+//     var element = document.getElementById('body');
+//
+//     for (var i = 5; i < 0; i--){
+//       element.style.backgroundImage = "url(../img/Fond" + i + ".jpg)"
+//     }
+// }
