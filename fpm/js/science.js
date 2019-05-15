6ef0915f4e5d864a9830807f6b7df5146b993183
Dima K <lusdjke@gmail.com>
2019-05-15 18:33:50 +0800

    123
    123

--- fpm/js/science.js
+++
@@ -1,9 +0,0 @@
-resizeScience();
-window.addEventListener('resize', resizeScience);
-function resizeScience(){
-  var image = document.getElementById('scienceImage');
-  if (window.screen.width > 490) {
-    image.src = 'img/the-lemon-exercise-desktop.jpg';
-  }
-  else image.src = 'img/the_lemon_exercise_mobile.jpg';
-}
