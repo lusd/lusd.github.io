0b5d08b2cd307ea584824d1c8647111e121ce61d
Dima K <lusdjke@gmail.com>
2019-11-14 14:45:14 +0800

    some changes

--- animation/js/animation.js
+++ animation/js/animation.js
@@ -1,46 +1,75 @@
 const planesNode = document.querySelectorAll('.plane');
 const button = document.querySelector('#slideDown');
-let planes = [];
 const redPlane = document.querySelector('.redPlane');
 const header = document.querySelector('.planes__header');
 const text = document.querySelector('.planes__text');
+const planesPathsNode = document.querySelectorAll('.path path');
 
+let planes = [];
 planesNode.forEach(plane => {
   return planes.push(plane)
 });
 
-const endWhitePlaneAnimation = () => {
-  planes.map(plane => {
-    plane.classList.add('offsetDistance1');
-    plane.classList.remove('animate');
-  })
-  startRedPlane();
-}
+const paths = [];
+planesPathsNode.forEach(path => {
+  return paths.push(path);
+});
 
 const endRedPlaneAnimation = () => {
-  redPlane.classList.add('offsetDistance95');
-  redPlane.classList.remove('redPlaneAnimate');
   window.scrollBy({
     top: 300,
     behavior: 'smooth'
   });
 }
 
-const startRedPlane = () => {
-  redPlane.classList.add('redPlaneAnimate');
-  setTimeout(endRedPlaneAnimation, 2000);
+const startWhitePlanes = () => {
+  for (var i = 0; i < planes.length; i++) {
+    var path = anime.path(paths[i]);
+    anime({
+      targets: planes[i],
+      translateX: path('x'),
+      translateY: path('y'),
+      rotate: path('angle'),
+      easing: 'easeInCubic',
+      duration: 2000,
+      loop: 1,
+      direction: 'reverse'
+    });
+  }
+
+  var path = anime.path('.redPath path');
+  var tl = anime.timeline({
+    targets: redPlane,
+    easing: 'easeOutCubic',
+    loop: 1
+  });
+
+  tl.add({
+    duration: 1,
+    opacity: 1,
+    delay: 500
+  })
+  .add({
+    translateX: path('x'),
+    translateY: path('y'),
+    rotate: path('angle'),
+    duration: 2000,
+  })
+  .add({
+    delay: 0,
+    duration: 400,
+    rotate: '12.5deg'
+  });
+  
+  tl.finished.then(endRedPlaneAnimation);
 }
 
 const onbuttonClick = (e) => {
   window.scrollBy({
-    top: 200,
+    top: 400,
     behavior: 'smooth'
   });
-  planes.map(plane => {
-    plane.classList.add('animate');
-  });
-  redPlane.classList.remove('offsetDistance95');
-  setTimeout(endWhitePlaneAnimation, 2000);
+  startWhitePlanes();
   header.style.opacity = "1";
   header.style.transform = 'translate(0, 0px)';
   text.style.opacity = "1";
