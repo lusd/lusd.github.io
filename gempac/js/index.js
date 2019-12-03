e0f5e03d2fa0abfe85a30405717e697ffe5caf79
Dima K <lusdjke@gmail.com>
2019-12-03 10:50:52 +0800

    bug report changes
     v.0.1

--- gempac/js/index.js
+++ gempac/js/index.js
@@ -1,8 +1,16 @@
-const toggleMenu = (button) => {
-  let nav = document.querySelector('.navigation');
-  nav.classList.toggle('active');
-  setTimeout( () => {
-    nav.classList.toggle('animation'); 
-  }, 015);
-  button.classList.toggle('change');
-}
+var card1 = document.getElementById('card1');
+var card2 = document.getElementById('card2');
+var card3 = document.getElementById('card3');
+window.addEventListener('resize', toggleCard);
+function toggleCard(){
+  if (window.innerWidth < 839) {
+    card1.setAttribute('src', "./img/company_mobile.png");
+    card2.setAttribute('src', "./img/services_mobile.png");
+    card3.setAttribute('src', "./img/clients_mobile.png");
+  } else {
+    card1.setAttribute('src', "./img/company.jpg");
+    card2.setAttribute('src', "./img/services.jpg");
+    card3.setAttribute('src', "./img/clients.jpg");
+  }
+};
+toggleCard();
