aff611861ec8ec9d276452b55f8591b1ddba9a82
Dima K <lusdjke@gmail.com>
2019-12-14 08:56:25 +0700

    asdf

--- gempac/js/navigation.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu(button) {
-  var nav = document.querySelector('.navigation');
-  nav.classList.toggle('active');
-  setTimeout( () => {
-    nav.classList.toggle('animation');
-  }, 015);
-  button.classList.toggle('change');
-}
